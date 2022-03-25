b3c73eb7d82483211de00ae35102346392e94342
HandsomeWalker <dreamcreativehub@gmail.com>
2022-03-25 10:57:36 +0800

    feat:remove psn

--- index.js
+++ index.js
@@ -1,45 +1,63 @@
 const EPIC_URL = "https://www.epicgames.com/store/zh-CN/free-games";
-const PSN_URL = "https://store.playstation.com/zh-hant-hk/home/games/psplus";
+// const PSN_URL = "https://store.playstation.com/zh-hant-hk/home/games/psplus";
 
 function loading() {
-    const html = `<div class="loading">
+  const html = `<div class="loading">
     <div><span></span></div>
     <div><span></span></div>
     <div><span></span></div>
 </div>`;
-    document.body.innerHTML = html;
+  document.body.innerHTML = html;
 }
 function open(url) {
-    utools.shellOpenExternal(url);
+  utools.shellOpenExternal(url);
 }
 function getData() {
-    loading();
-    utools.subInputFocus();
-    let html = '<h2>Epic免费游戏-<a id="epic-url" href="">点击领取</a></h2><div class="flex-box">';
-    const p1 = window.getPsnData();
-    const p2 = window.getEpicData();
-    Promise.all([p1, p2]).then(([res1, res2]) => {
-        const games = res2.data.Catalog.searchStore.elements;
-        games.forEach(item => {
-            const { title, keyImages } = item;
-            // let img = keyImages.filter(each => each.type === "Thumbnail");
-            const img = keyImages[0] ? keyImages[0].url : "";
-            html += 
-            `<div>
+  loading();
+  utools.subInputFocus();
+  let html = '<h2>Epic免费游戏-<a id="epic-url" href="">去官网领取</a></h2><div class="flex-box">';
+  // const p1 = window.getPsnData();
+  const p2 = window.getEpicData();
+  // Promise.all([p1, p2]).then(([res1, res2]) => {
+  //     const games = res2.data.Catalog.searchStore.elements;
+  //     games.forEach(item => {
+  //         const { title, keyImages } = item;
+  //         // let img = keyImages.filter(each => each.type === "Thumbnail");
+  //         const img = keyImages[0] ? keyImages[0].url : "";
+  //         html +=
+  //         `<div>
+  //     <img src="${img}" width="124" height="124" />
+  //     <div>${title}</div>
+  // </div>`
+  //     });
+  //     html += '</div><h2>PS4港服会免游戏-<a id="psn-url" href="">点击领取</a></h2><div class="flex-box">' + res1 + '</div>';
+  //     document.body.innerHTML = html;
+  //     document.body.style.backgroundColor = "#ffffff";
+  //     setTimeout(function() {
+  //         document.querySelector("#epic-url").addEventListener("click", open.bind(null, EPIC_URL));
+  //         document.querySelector("#psn-url").addEventListener("click", open.bind(null, PSN_URL));
+  //     }, 10);
+  // }).catch(err => {
+  //     alert(err)
+  // });
+  p2.then(res2 => {
+    const games = res2.data.Catalog.searchStore.elements;
+    games.forEach(item => {
+      const { title, keyImages } = item;
+      // let img = keyImages.filter(each => each.type === "Thumbnail");
+      const img = keyImages[0] ? keyImages[0].url : "";
+      html +=
+        `<div>
         <img src="${img}" width="124" height="124" />
         <div>${title}</div>
     </div>`
-        });
-        html += '</div><h2>PS4港服会免游戏-<a id="psn-url" href="">点击领取</a></h2><div class="flex-box">' + res1 + '</div>';
-        document.body.innerHTML = html;
-        document.body.style.backgroundColor = "#ffffff";
-        setTimeout(function() {
-            document.querySelector("#epic-url").addEventListener("click", open.bind(null, EPIC_URL));
-            document.querySelector("#psn-url").addEventListener("click", open.bind(null, PSN_URL));
-        }, 10);
-    }).catch(err => {
-        alert(err)
     });
+    document.body.innerHTML = html;
+    document.body.style.backgroundColor = "#ffffff";
+    setTimeout(function () {
+      document.querySelector("#epic-url").addEventListener("click", open.bind(null, EPIC_URL));
+    }, 10);
+  });
 }
 
 utools.onPluginEnter(getData);
--- preload.js
+++ preload.js
@@ -1,5 +1,5 @@
 const https = require("https");
-const cheerio = require("./cheerio");
+// const cheerio = require("./cheerio");
 
 const UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36";
 
@@ -38,25 +38,25 @@ window.getEpicData = function() {
     })
 }
 
-window.getPsnData = function() {
-    return new Promise((resolve, reject) => {
-        request({
-            hostname: "store.playstation.com",
-            path: "/zh-hant-hk/home/games/psplus",
-            port: 443,
-            method: "GET",
-            headers: {
-                "user-agent": UA
-            }
-        }, function(err, body) {
-            if (err) {
-                reject(err);
-            }
-            const $ = cheerio.load(body);
-            const html = $(".grid-cell-row__container-name").filter(function() {
-                return $(this).text() === "免費遊戲";
-            }).parent().next().html();
-            resolve(html);
-        })
-    })
-}
+// window.getPsnData = function() {
+//     return new Promise((resolve, reject) => {
+//         request({
+//             hostname: "store.playstation.com",
+//             path: "/zh-hant-hk/home/games/psplus",
+//             port: 443,
+//             method: "GET",
+//             headers: {
+//                 "user-agent": UA
+//             }
+//         }, function(err, body) {
+//             if (err) {
+//                 reject(err);
+//             }
+//             const $ = cheerio.load(body);
+//             const html = $(".grid-cell-row__container-name").filter(function() {
+//                 return $(this).text() === "免費遊戲";
+//             }).parent().next().html();
+//             resolve(html);
+//         })
+//     })
+// }
